954d807498f89f381fde8c9ae8fda5e8afdb6c23
dacapo <dacapo1142@gmail.com>
2017-04-11 21:30:45 +0800

    auto install

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+env git clone --depth=1 git@github.com:dacapo1142/PTTfeeds.git || {
+    printf "\e[31mError: git clone of PTTfeeds repo failed\n\e[0m"
+    exit 1
+}
+
+env pip install -U -r requirements.txt  ||
+{
+    printf "\e[31mError: pip install requirements.txt repo failed\n\e[0m"
+    exit 1
+}
